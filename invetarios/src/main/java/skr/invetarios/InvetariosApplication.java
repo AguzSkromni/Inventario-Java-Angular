354c4882673154f9c601cee8e10e54c75b28b703
Agustin Reyes Hernandez <agustin.reyesh95@gmail.com>
2024-08-12 14:23:38 -0600

    Inicio del Proyecto utilizando Spring Initializr
    Se desplego el proyecto con Spring Initializr para empezar la configuracion de los archivos originales de la primera instancia.
    
    Las dependencias que se agregaron son:
    -Spring Web
    -Spring Data JPA
    -MySQL Driver
    -Lombok

---
+++ invetarios/src/main/java/skr/invetarios/InvetariosApplication.java
@@ -0,0 +1,13 @@
+package skr.invetarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvetariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvetariosApplication.class, args);
+	}
+
+}
---
+++ invetarios/src/test/java/skr/invetarios/InvetariosApplicationTests.java
@@ -0,0 +1,13 @@
+package skr.invetarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvetariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
